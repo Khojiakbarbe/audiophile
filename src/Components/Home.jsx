18b558bade63750fbbb035dd12bdc7288884bded
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-05-17 08:10:55 +0500

    .

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,18 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 
 
-export default function Home(){
-    return(
+export default function Home() {
+
+    const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     axios.get()
+    // })
+
+
+    return (
         <h1>Home</h1>
     )
 }
