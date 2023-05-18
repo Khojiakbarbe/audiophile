da5b0e3530193a4ea6930911a42678f650b7a7cc
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-05-18 23:04:16 +0500

    footer added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom'
+import { FaFacebookSquare } from 'react-icons/fa'
+import { AiOutlineTwitter } from 'react-icons/ai'
+import { AiOutlineInstagram } from 'react-icons/ai'
+
+
 
 export default function Footer() {
     return (
         <div className="footer">
             <div className="row">
                 <div className="col-md-6">
-                    <h3 style={{color:'white'}}><strong>audiophile</strong></h3>
+                    <h3 style={{ color: 'white' }}><strong>audiophile</strong></h3>
                     <p>Audiophile is an all in one stop to fulfill your audio needs. We're a small team <br /> of music lovers and sound specialists who are devoted to helping you get the <br /> most out of personal audio. Come and visit our demo facility - we’re open <br /> 7 days a week.</p>
                     <p>Copyright 2021. All Rights Reserved</p>
                 </div>
@@ -16,6 +21,11 @@ export default function Footer() {
                         <li><Link to='/' className="nav-link px-2 text-white" >SPEAKERS</Link></li>
                         <li><Link to='/' className="nav-link px-2 text-white" >EARPHONES</Link></li>
                     </ul>
+                    <div className="icons">
+                        <FaFacebookSquare className='icon' />
+                        <AiOutlineTwitter className='icon'/>
+                        <AiOutlineInstagram className='icon' />
+                    </div>
                 </div>
             </div>
         </div>
