c94620d6acec74f2cded870e9a2c00dbcb8dc8bd
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-05-19 00:00:29 +0500

    smth added

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react"
 import axios from "axios"
 
 import carousel from '../images/home/Bitmap.png'
+import bg from '../images/home/bg.png'
+import speaker1 from '../images/home/speakerZX9.png'
+import speaker2 from '../images/home/speakerZX7.png'
+import earphone from '../images/home/earphone.png'
 
 
 export default function Home() {
@@ -37,7 +41,7 @@ export default function Home() {
             <div className="menu">
                 <div className="row">
                     {
-                        data && data.slice(2,5).map(post => {
+                        data && data.slice(2, 5).map(post => {
                             return (
                                 <div className="col-md-4">
                                     <div className="card">
@@ -49,6 +53,38 @@ export default function Home() {
                             )
                         })
                     }
+                </div>
+            </div>
+
+            <div className="container rec">
+                <div className="row mt-5 mb-5 zx9">
+                    <div className="col-md-6">
+                        <img src={speaker1} className='img-fluid' alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h1>ZX9 <br /> SPEAKER</h1>
+                        <p>Upgrade to premium speakers that are <br /> phenomenally built to deliver truly remarkable <br /> sound.</p>
+                        <button className="seeProduct">SEE PRODUCT</button>
+                    </div>
+                </div>
+                <div className="row mt-5 mb-5 zx7">
+
+                    <div className="col-md-12">
+                        <div className="zx7Text">
+                            <h1>ZX9 <br /> SPEAKER</h1>
+                            <button className="seeProduct">SEE PRODUCT</button>
+                        </div>
+                        {/* <img src={speaker2} className='w-100' alt="" /> */}
+                    </div>
+                </div>
+                <div className="row mt-5 mb-5 earphone">
+                    <div className="col-md-6">
+                        <img src={earphone} className='img-fluid card' alt="" />
+                    </div>
+                    <div className="col-md-6">
+                        <h1>YX1 EARPHONES</h1>
+                        <button className="seeProduct">SEE PRODUCT</button>
+                    </div>
                 </div>
             </div>
 
