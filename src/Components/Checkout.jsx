6383ee61a7b1dcca0f700e5479f21f53d7cf3f57
Khojiakbarbe <121534562+Khojiakbarbe@users.noreply.github.com>
2023-06-04 19:38:03 +0500

    smth added to checkout page

--- src/Components/Checkout.jsx
+++ src/Components/Checkout.jsx
@@ -7,100 +7,127 @@ export default function Checkout() {
 
     const state = useLocation();
 
+    // const filtered = [];
+    // if (productsCon.length > 0) {
+    //     for (let i = 0; i < productsCon.length; i++) {
+    //         const filter = data.filter(find => find.id == productsCon[i].id)
+    //         if (filter) {
+    //             filtered.push({ data: filter, count: productsCon[i].count })
+    //         }
+    //     }
+    // }
+
+    // const [allPrice, setAllPrice] = useState(0);
+    // function prices() {
+    //     if (filtered.length > 0) {
+    //         let prices = 0;
+    //         for (let i = 0; i < filtered.length; i++) {
+    //             prices += filtered[i].data[0].price * filtered[i].count;
+    //         }
+    //         setAllPrice(prices)
+    //     }
+    // }
+
+    let total = 0;
+    state.state.forEach(element => {
+        total += element.data[0].price * element.count
+    });
     return (
         <div className="container pt-5 pb-5">
             <p onClick={() => navigate(-1)} className='btn'>Go Back</p>
-            <div className="row">
-                <div className="col-8 border pb-5">
-                    <h1>CHECKOUT</h1>
-                    <h5 className='checkoutH5'>BILLING DETAILS</h5>
-                    <div className="row mb-3">
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>Name</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='Alexei Ward' className='form-control mt-2 p-3' />
+            <div className="row w-100">
+                <div className="col-sm-8 p-2">
+                    <div className="cards">
+                        <h1>CHECKOUT</h1>
+                        <h5 className='checkoutH5'>BILLING DETAILS</h5>
+                        <div className="row mb-3">
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>Name</strong></label>
+                                <br />
+                                <input type="text" id='name' placeholder='Alexei Ward' className='form-control mt-2 p-3' />
+                            </div>
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>Email Address</strong></label>
+                                <br />
+                                <input type="text" id='name' placeholder='alexei@mail.com' className='form-control mt-2 p-3' />
+                            </div>
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>Phone Number</strong></label>
+                                <br />
+                                <input type="Number" id='name' placeholder='+1 202-555-0136' className='form-control mt-2 p-3' />
+                            </div>
                         </div>
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>Email Address</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='alexei@mail.com' className='form-control mt-2 p-3' />
-                        </div>
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>Phone Number</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='+1 202-555-0136' className='form-control mt-2 p-3' />
-                        </div>
-                    </div>
 
-                    <h5 className='checkoutH5'>SHIPPING INFO</h5>
-                    <label htmlFor="adress"><strong>Adress</strong></label>
-                    <input type="text" placeholder='1137 Williams Avenue' className='form-control p-3 mb-4 mt-3' id="adress" />
-                    <div className="row mb-5">
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>ZIP Code</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='10001' className='form-control mt-2 p-3' />
+                        <h5 className='checkoutH5'>SHIPPING INFO</h5>
+                        <label htmlFor="adress"><strong>Adress</strong></label>
+                        <input type="text" placeholder='1137 Williams Avenue' className='form-control p-3 mb-4 mt-3' id="adress" />
+                        <div className="row mb-5">
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>ZIP Code</strong></label>
+                                <br />
+                                <input type="text" id='name' placeholder='10001' className='form-control mt-2 p-3' />
+                            </div>
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>City</strong></label>
+                                <br />
+                                <input type="text" id='name' placeholder='New York' className='form-control mt-2 p-3' />
+                            </div>
+                            <div className="col-md-6 mb-2">
+                                <label htmlFor="name"><strong>Country</strong></label>
+                                <br />
+                                <input type="text" id='name' placeholder='United States' className='form-control mt-2 p-3' />
+                            </div>
                         </div>
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>City</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='New York' className='form-control mt-2 p-3' />
-                        </div>
-                        <div className="col-md-6 mb-2">
-                            <label htmlFor="name"><strong>Country</strong></label>
-                            <br />
-                            <input type="text" id='name' placeholder='United States' className='form-control mt-2 p-3' />
-                        </div>
-                    </div>
 
-                    <h5 className='checkoutH5'>PAYMENT DETAILS</h5>
-                    <div className="row">
-                        <div className="col-md-6">
-                            <p><strong>Payemnt Method</strong></p>
-                        </div>
-                        <div className="col-md-6 mb-5">
-                            <div className='mb-3 p-2'>
+                        <h5 className='checkoutH5'>PAYMENT DETAILS</h5>
+                        <div className="row">
+                            <div className="col-md-6">
+                                <p><strong>Payemnt Method</strong></p>
+                            </div>
+                            <div className="col-md-6 mb-5">
                                 <input type="radio" id='e-moneyR' name='payMethod' className="payMetRadio" placeholder='e-Money' />
                                 <label htmlFor="e-moneyR">e-Money</label>
-                            </div>
-                            <div >
+                                <br />
                                 <input type="radio" id='cashDeliveryR' className="payMetRadio" name='payMethod' placeholder='Cash on Delivery' />
                                 <label htmlFor="cashDeliveryR">Cash on Delivery</label>
                             </div>
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="e-Money"><strong>e-Money</strong></label>
-                            <input type="text" id='e-Money' placeholder='238521993' className='form-control p-3' />
-                        </div>
-                        <div className="col-md-6">
-                            <label htmlFor="e-MoneyPin"><strong>e-Money PIN</strong></label>
-                            <input type="text" id='e-MoneyPin' placeholder='6891' className='form-control p-3' />
+                            <div className="col-md-6">
+                                <label htmlFor="e-Money"><strong>e-Money</strong></label>
+                                <input type="text" id='e-Money' placeholder='238521993' className='form-control p-3' />
+                            </div>
+                            <div className="col-md-6">
+                                <label htmlFor="e-MoneyPin"><strong>e-Money PIN</strong></label>
+                                <input type="text" id='e-MoneyPin' placeholder='6891' className='form-control p-3' />
+                            </div>
                         </div>
                     </div>
 
                 </div>
-                <div className="col-4">
-                    <h3>SUMMARY</h3>
-                    {state.state.map((post, ind) => {
-                        return (
-                            <div key={ind} className='row'>
-                                <div className="col-md-8 row">
-                                    <div className="col-sm-4">
-                                        <img src={post.data[0].image.mobile} width='50' alt="" />
+                <div className="col-sm-4 p-2">
+                    <div className="cards">
+                        <h3>SUMMARY</h3>
+                        {state.state.map((post, ind) => {
+                            return (
+                                <div key={ind} className='row'>
+                                    <div className="col-md-8 row">
+                                        <div className="col-sm-4">
+                                            <img src={post.data[0].image.mobile} width='50' alt="" />
+                                        </div>
+                                        <div className="col">
+                                            <p>{post.data[0].name}</p>
+                                            <p style={{ color: 'gray' }}>${post.data[0].price}</p>
+                                        </div>
                                     </div>
                                     <div className="col">
-                                        <p>{post.data[0].name}</p>
-                                        <p style={{ color: 'gray' }}>${post.data[0].price}</p>
+                                        <p style={{ color: 'gray' }}><strong>x{post.count}</strong></p>
                                     </div>
+                                    <h4 id='total'><strong>${total}</strong></h4>
                                 </div>
-                                <div className="col">
-                                    <p style={{ color: 'gray' }}><strong>x{post.count}</strong></p>
-                                </div>
-                            </div>
-                        )
-                    })}
-                    <div className='text-center'>
-                        <button className='seeAndAddToCard'>CONTINUE & PAY</button>
+                            )
+                        })}
+                        <div className='text-center'>
+                            <button className='seeAndAddToCard'>CONTINUE & PAY</button>
+                        </div>
                     </div>
                 </div>
             </div>
